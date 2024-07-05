e8d42a63ab8ce1725aee6582baa299269b105ef1
JJkasdae <hjj2215975331@gmail.com>
2024-07-05 17:14:39 +1000

    Create a new function in the menu item for presentation creation.
    Separate user data which is created using the editor from the functionality codes. There are two folders now. IPTK includes all functionalities, the other one includes all data created by users.

--- IPTK/Assets/IPTK/Script/CreatePresentationElements.cs
+++ IPTK/Assets/IPTK/Script/CreatePresentationElements.cs
@@ -3,15 +3,17 @@ using UnityEngine;
 using UnityEditor;
 using UnityEngine.SceneManagement;
 using UnityEditor.SceneManagement;
+using System;
+using System.IO;
 
 public class CreatePresentationElements
 {
-    [MenuItem("ScriptableObject/Create Session and Transition")]
+    [MenuItem("PresentationData/Create sessions, transitions and scenes")]
     public static void CreateAllObjects()
     {
         Scene newScene = EditorSceneManager.NewScene(NewSceneSetup.DefaultGameObjects, NewSceneMode.Single);
         newScene.name = "NameScene";
-        EditorSceneManager.SaveScene(newScene, "Assets//IPTK/Scenes/NewScene.unity");
+        EditorSceneManager.SaveScene(newScene, "Assets/Presentation/NewScene.unity");
         AssetDatabase.SaveAssets();
 
         SessionData newSession = ScriptableObject.CreateInstance<SessionData>();
@@ -25,6 +27,42 @@ public class CreatePresentationElements
         AssetDatabase.SaveAssets();
 
         AssetDatabase.Refresh();        
+    }
+}
+
+public class CreatePresentations
+{
+    private const string folderPath = "Assets/Data/PresentationObject";
+
+    [MenuItem("PresentationData/Create a presentation")]
+    public static void CreatePresentation()
+    {
+        // Check the folder is existed or not. Existed -> save the file to the folder. Not existed -> create a folder and save the file.
+        CheckFolderExists(folderPath);
+
+        // Create a presentation scriptable object
+        PresentationData newPresentation = ScriptableObject.CreateInstance<PresentationData>();
+        AssetDatabase.CreateAsset(newPresentation, AssetDatabase.GenerateUniqueAssetPath(Path.Combine(folderPath, "Presentation.asset")));
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+    }
+
+    private static void CheckFolderExists(string folderPath)
+    {
+        if (string.IsNullOrEmpty(folderPath) || AssetDatabase.IsValidFolder(folderPath))
+        {
+            return;
+        }
+
+        // Get the parent folder path
+        string parentFolder = Path.GetDirectoryName(folderPath);
+
+        // If parent folder is not created, recursive in the parent folder and create a new folder.
+        CheckFolderExists(parentFolder);
+
+        // Create the required folder
+        string newFoldername = Path.GetFileName(folderPath);
+        AssetDatabase.CreateFolder(parentFolder, newFoldername);
     }
 }
 
