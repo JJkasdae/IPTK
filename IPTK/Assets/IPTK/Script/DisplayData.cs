e9f6155cf7101b1741fe55ce8b49d9645ffb4f80
JJkasdae <hjj2215975331@gmail.com>
2024-07-10 14:22:19 +1000

    Display data prefab should find the reference to the main function and get the session data from there.

--- IPTK/Assets/IPTK/Script/DisplayData.cs
+++ IPTK/Assets/IPTK/Script/DisplayData.cs
@@ -2,21 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DisplayData : MonoBehaviour
 {
-    [SerializeField]
-    private SessionData _sessionData;
-
     [SerializeField]
     private TextMeshProUGUI _nameText;
 
     [SerializeField]
     private TextMeshProUGUI _descriptionText;
 
+    [SerializeField]
+    private PresentationData _presentationData;
+
+    private SessionData _sessionData;
+    private string _currentSceneName;
+
     // Start is called before the first frame update
     void Start()
     {
+        _currentSceneName = SceneManager.GetActiveScene().name;
+
+        for (int i = 0; i < _presentationData.Timeline.transitionData.Length; i++)
+        {
+            if (_presentationData.Timeline.transitionData[i].lastSession.sceneName == _currentSceneName)
+            {
+                _sessionData = _presentationData.Timeline.transitionData[i].lastSession;
+                break;
+            }
+        }
+
         if (_sessionData != null)
         {
             _nameText.text = _sessionData.Name;
